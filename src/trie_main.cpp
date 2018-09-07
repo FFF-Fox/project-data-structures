aafd2265d964bbee38f194a6e2891d24e9e6f24b
FFF-Fox <fff-fox@protonmail.com>
2018-09-07 12:13:15 +0300

    Added test.

--- src/trie_main.cpp
+++ src/trie_main.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "Cli.h"
 #include "Trie.h"
 
-void make_trie_cli(cli::Cli& trie_cli, trie::Trie &t);
+#include <vector> /* testing */
 
-int main()
+void read_words(std::string filename, trie::Trie &t);
+void make_trie_cli(cli::Cli &trie_cli, trie::Trie &t);
+
+void test_trie(std::string filename); /* testing */
+
+int main(int argc, char *argv[])
 {
-    trie::Trie t;
-    cli::Cli trie_cli;
+    if (argc != 2)
+    {
+        std::cerr << "Παρακαλώ εισάγετε το path του αρχείου με τις λέξεις για εισαγωγή στο trie..." << std::endl;
+        std::cerr << "Χρήση: " << argv[0] << " FILENAME" << std::endl;
 
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    trie::Trie t;
+
+    read_words(filename, t);
+
+    cli::Cli trie_cli;
     make_trie_cli(trie_cli, t);
     trie_cli.start();
 
     return 0;
 }
 
-void make_trie_cli(cli::Cli& trie_cli, trie::Trie &t)
+void read_words(std::string filename, trie::Trie &t)
+{
+    /* open file */
+    std::ifstream infile(filename);
+
+    /* for each line:
+        1. parse string
+        2. insert to trie */
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        t.insert(line);
+    }
+}
+
+void make_trie_cli(cli::Cli &trie_cli, trie::Trie &t)
 {
     std::string menu =
         R"(1 - αναζήτηση
@@ -64,4 +97,43 @@ q - έξοδος)";
         t.remove(str);
         std::cout << context << "Έγινε διαγραφή." << std::endl;
     });
+}
+
+void test_trie(std::string filename)
+{
+    trie::Trie t;
+
+    /* open file */
+    std::ifstream infile(filename);
+    std::vector<std::string> words;
+
+    std::string line;
+    while (std::getline(infile, line))
+    {
+        words.push_back(line);
+        t.insert(line);
+    }
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        if (!t.find(words[i]))
+        {
+            std::cout << words[i] << " not found!" << std::endl;
+            return;
+        }
+    }
+    std::cout << "All words found!" << std::endl;
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        t.remove(words[i]);
+        if (t.find(words[i]))
+        {
+            std::cout << words[i] << " wasn't removed successfully!" << std::endl;
+            return;
+        }
+    }
+    std::cout << "All words removed successfully!" << std::endl;
+
+    std::cout << "All tests passed." << std::endl;
 }
