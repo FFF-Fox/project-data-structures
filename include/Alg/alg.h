1727693c18c2aab66a007a904e375575bc7eb577
FFF-Fox <fff-fox@protonmail.com>
2018-09-07 14:18:51 +0300

    Initialized the sort and search part of the project.

---
+++ include/Alg/alg.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+namespace alg
+{
+std::vector<int> mergesort(std::vector<int> Arr);
+
+int linear_search(std::vector<int> Arr);
+int binary_search(std::vector<int> Arr);
+int interpolation_search(std::vector<int> Arr);
+}
---
+++ src/search_main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "alg.h"
+
+void test_mergesort();
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
+
+void test_mergesort()
+{
+}
